47ade91aa6e47dd953d9e1f55542d4a4f13d85fc
Period 6 <braylen.diec@talawanda.org>
2024-02-07 14:04:45 -0500

    204

--- src/Task1.java
+++ src/Task1.java
@@ -2,6 +2,17 @@ public class Task1 {
     public static void main(String[] args){
 
         //Variables
+        double price = 19.99;
+        double salesTaxPrice;
+        double total;
+        double SALESTAX = 0.05;
+
+        //Math
+        salesTaxPrice = price * SALESTAX;
+        total = price + salesTaxPrice;
+
+        //Output
+        System.out.println("The sales tax is: $" + salesTaxPrice + "\n The total is: $" + total);
 
 
 
