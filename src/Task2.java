daf1cd2708d373ea309ecb65e05feeee0a703462
Period 6 <braylen.diec@talawanda.org>
2024-02-08 13:35:24 -0500

    135

---
+++ src/Task2.java
@@ -0,0 +1,16 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        //Variables
+        double seasonOne, seasonTwo, seasonThree, seasonFour, total;
+        seasonOne = 4000.32;
+        seasonTwo = 2900.56;
+        seasonThree = 10000.86;
+        seasonFour = 1800.65;
+        total = seasonOne + seasonTwo + seasonThree + seasonFour;
+
+        //Output
+        System.out.println("Your maintenance costs are: \n Season One: $" + seasonOne + "\n Season Two: $" + seasonTwo + "\n Season Three: $" + seasonThree + "\n Season Four: $" + seasonFour + "\nTotal: $" + total);
+
+    }
+}
